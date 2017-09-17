972d4264fba838556b8e965ffb110203ff03d251
User <User@ArnaldoPC>
2017-09-18 00:47:11 +0530

    Final

--- src/Data/ConnectionClass.java
+++ src/Data/ConnectionClass.java
@@ -16,14 +16,27 @@ public class ConnectionClass
 {
     public java.sql.Connection getConnection(){
     java.sql.Connection con=null;
+//    try
+//    {
+//       Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+//        con=DriverManager.getConnection("jdbc:odbc:deg_final","","");
+//    }
+//    catch(Exception e){
+//        JOptionPane.showMessageDialog(null,new String ("Error Connecting To Database "+e));
+//    }
+//    return con;
+    
+    String url = "jdbc:postgresql://localhost:5432/voice_reg";
     try
-    {
-       Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-        con=DriverManager.getConnection("jdbc:odbc:deg_final","","");
-    }
-    catch(Exception e){
-        JOptionPane.showMessageDialog(null,new String ("Error Connecting To Database "+e));
-    }
+        {
+            Class.forName("org.postgresql.Driver");
+            
+            con = DriverManager.getConnection(url,"postgres","admin");
+        }
+        catch (Exception e1)
+        {
+            JOptionPane.showMessageDialog(null, "Error "+e1.toString());
+        }
     return con;
 }    
 }
